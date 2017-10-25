fad9989dbc91049fe99174c0c49e66569f71258e
Sebastian Grobelny <sgrobelny@Sebastians-MacBook-Air-2.local>
2017-10-25 14:54:46 -0700

    First commit with Django CaWineclassics

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 DATABASES = {
     'default': {
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
