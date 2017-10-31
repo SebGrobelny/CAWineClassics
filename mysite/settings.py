a2d9a00370aeda10ff300e32bb46e0738d9772fa
Sebastian Grobelny <sgrobelny@Sebastians-MacBook-Air-2.local>
2017-10-31 11:46:35 -0700

    Updated search request

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-# DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
